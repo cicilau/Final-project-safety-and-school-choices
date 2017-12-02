4d1e33b17f773a29cde6e6d644edde00eb7edb5a
cicilau <32332007+cicilau@users.noreply.github.com>
2017-12-01 19:42:47 -0600

    CODE

--- act.py
+++ act.py
@@ -3,13 +3,13 @@ import pandas as pd
 import urllib.request as request
 ...
 # Download the file
-request.urlretrieve('http://cps.edu/Performance/Documents/Datafiles/AverageACT_2016_SchoolLevel.xls', 'data/raw_act.xls')
+request.urlretrieve('http://cps.edu/Performance/Documents/Datafiles/AverageACT_2016_SchoolLevel.xls', 'DATA/raw_act.xls')
 
 # Import raw csv data with all schools
-raw_data = pd.read_excel('data/raw_act.xls', sheet_name='act_schools_2001_to_2016',
+raw_data = pd.read_excel('DATA/raw_act.xls', sheet_name='act_schools_2001_to_2016',
     header=0, skiprows=1, skipfooter=2)
 
 
 overall_data = raw_data[raw_data['Category']=='Overall']
 refined_data = overall_data[['School ID', 'Year', 'Composite']]
-refined_data.to_csv('data/refined_act.csv', index=False)
+refined_data.to_csv('DATA/refined_act.csv', index=False)
