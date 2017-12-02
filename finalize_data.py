4131c7ab709fc5d81e253caee612bff547a12713
cicilau <32332007+cicilau@users.noreply.github.com>
2017-12-01 20:02:34 -0600

    CODE

---
+++ finalize_data.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+schools = pd.read_csv('DATA/refined_progress.csv')
+
+crimes_all = pd.read_csv('DATA/school_crimes.csv')
+
+
+crimes_by_school = crimes_all.groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Crimes':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==0].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Thefts':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==1].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Batteries':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==4].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Assaults':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==7].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Robberies':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==16].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Weapon_Violations':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+crimes_by_school = crimes_all[crimes_all['Crime_Type']==20].groupby(['School_ID']).size()
+crimes = pd.DataFrame({'School_ID':crimes_by_school.index, 'Total_Homicides':crimes_by_school.values})
+schools = schools.merge(crimes, how='outer', on=['School_ID'])
+
+act_all = pd.read_csv('DATA/refined_act.csv')
+act = act_all[act_all['Year']==2016][['School ID','Composite']] \
+    .rename(index=str, columns={'School ID':'School_ID', 'Composite':'ACT_Score'})
+schools = schools.merge(act, how='outer', on=['School_ID'])
+
+college = pd.read_csv('DATA/refined_college.csv') \
+    .rename(index=str, columns={'School ID':'School_ID', 'Enrollment Pct':'College_Enrollment_Pct'})
+schools = schools.merge(college, how='outer', on=['School_ID'])
+
+grad = pd.read_csv('DATA/refined_graduation.csv') \
+    .rename(index=str, columns={'SchoolID':'School_ID', '2017.1':'Graduation_Pct'})
+schools = schools.merge(grad, how='outer', on=['School_ID'])
+
+level = pd.read_csv('DATA/ratings.csv')
+schools = schools.merge(level, how='outer', on=['School_ID'])
+
+schools.to_csv('DATA/data_table.csv', index=False)
